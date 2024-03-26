fee566d0bad95e98793bfd4f5343a1fba55d21f0
Somnath Gupta <sapphelper@gmail.com>
2024-03-26 15:38:28 +0530

    update base and apibaseurl

--- Frontend/constant.ts
+++ Frontend/constant.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://kohlerfrontend.vercel.app";
+export const BASE_URL = "https://premiumbathware.com";
--- Frontend/utils/API_BASE_URL.ts
+++ Frontend/utils/API_BASE_URL.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://kohlerbackend.vercel.app";
+export const API_BASE_URL = "https://kohler-black.vercel.app";

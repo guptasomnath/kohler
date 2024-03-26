37435617360fc04cebde286a9e1f397e9b6971b4
Somnath Gupta <sapphelper@gmail.com>
2024-03-26 16:24:44 +0530

    add db info

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2/promise");
 
 async function query(sql, params) {
+  const HOST = process.env.DB_HOST;
+  const USER = process.env.DB_USER;
+  const PASS = process.env.DB_PASSWORD;
+  const DB = process.env.DATABASE;
   const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "kohler",
+    host: HOST,
+    user: USER,
+    password: PASS,
+    database: DB,
     connectTimeout: 60000,
   });
   const [results] = await connection.execute(sql, params);
-
   return results;
 }
 

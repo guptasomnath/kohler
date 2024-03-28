e3705ce64f75c3f5fca4484dbcb994e28033f616
Somnath Gupta <sapphelper@gmail.com>
2024-03-28 16:33:38 +0530

    update .env

--- Frontend/api/sendMail.ts
+++ Frontend/api/sendMail.ts
@@ -10,7 +10,7 @@ export async function sendMail(
 
   const url =
     script +
-    `?toEmail=locbilla@gmail.com&subject=User want to contact&body=<h4>Name : ${user_name}</h4><h4>Email : ${user_email}</h4><h4>Number : ${user_number}</h4><h4>Message : ${user_message}</h4>`;
+    `?toEmail=subrata.ash@gmail.com&subject=User want to contact&body=<h4>Name : ${user_name}</h4><h4>Email : ${user_email}</h4><h4>Number : ${user_number}</h4><h4>Message : ${user_message}</h4>`;
 
   const response = await fetch(url, { next: { revalidate: 360 } });
   const result = await response.text();

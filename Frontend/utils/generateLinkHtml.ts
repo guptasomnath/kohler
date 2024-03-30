c6327a096a11949f92941089904e7576a35d57e1
Somnath Gupta <sapphelper@gmail.com>
2024-03-30 18:53:34 +0530

    fix a bug of color not appling

--- Frontend/utils/generateLinkHtml.ts
+++ Frontend/utils/generateLinkHtml.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "@/constant";
 
 export const generateLinkHtml = (endpoint: string, text: string) => {
     // ${BASE_URL}/basin-area/basin
-    return `<a class = "text-blue-700 font-semibold" href = "${
+    return `<a style="color: #1d4ed8;" class = "font-semibold" href = "${
       BASE_URL + endpoint
     }">${text}</a>`;
   };

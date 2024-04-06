0adcd837140a5fb0a05ca8c6c11a5ef05d1c2c96
Somnath Gupta <sapphelper@gmail.com>
2024-04-06 13:27:50 +0530

    fixed accessibility problem for contact buttons

--- Frontend/app/components/FloatingBtn.tsx
+++ Frontend/app/components/FloatingBtn.tsx
@@ -26,20 +26,29 @@ function FloatingBtn() {
 
   return (
     <div className="absolute bottom-14 right-20 z-10 flex flex-col gap-4 items-center sm:right-6">
-      <h2 className={`absolute text-gray-600 w-32 px-5 py-1 right-10 bottom-12 messageShape flex items-center justify-center font-semibold bg-emerald-400 ${otherBtnVisibility ? "scale-0" : "scale-1"} transition-all duration-300`}>Contact us</h2>
+      <h2
+        className={`absolute w-32 px-5 text-white py-1 right-10 bottom-12 messageShape flex items-center justify-center font-semibold bg-emerald-400 ${
+          otherBtnVisibility ? "scale-0" : "scale-1"
+        } transition-all duration-300`}
+      >
+        Contact us
+      </h2>
       <RoundedBtn
+        title="Whatsapp Me Button"
         onClick={onCallBtnClick}
         className={`bg-blue-800 ${otherBtnVisibility ? "scale-1" : "scale-0"}`}
       >
         <IoCallSharp size={18} color="#fff" />
       </RoundedBtn>
       <RoundedBtn
+        title="Message Button"
         onClick={onMsgBtnClick}
         className={`bg-green-700 ${otherBtnVisibility ? "scale-1" : "scale-0"}`}
       >
         <LuMessagesSquare color="#fff" />
       </RoundedBtn>
       <RoundedBtn
+        title="Contact Us Button"
         className={`bg-gray-800 rounded`}
         onClick={onExpandBtnClick}
       >
--- Frontend/app/components/RoundedBtn.tsx
+++ Frontend/app/components/RoundedBtn.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface IProps {
   children?: React.ReactNode;
   className?: string;
+  title? : string;
   onClick?: () => void;
 }
 
-function RoundedBtn({ children, className, onClick }: IProps) {
+function RoundedBtn({ children, className, title, onClick }: IProps) {
   return (
     <button
+      title={title}
       onClick={onClick}
       className={`h-12 w-12 flexCenter rounded-full cursor-pointer hover:scale-105 transition-all duration-500 ${className}`}
     >
